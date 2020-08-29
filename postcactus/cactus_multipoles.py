4510be84baab63e8e596726eff694affaf0c9142
Gabriele Bozzola <sbozzolator@gmail.com>
2020-08-29 15:41:11 -0700

    Add MultipolesAllDets

--- postcactus/cactus_multipoles.py
+++ postcactus/cactus_multipoles.py
@@ -8,6 +8,10 @@
 """
 
 from postcactus import timeseries
+# import numpy as np
+# import h5py
+# import re
+# import os
 
 
 class MultipoleDet:
@@ -74,6 +78,12 @@ class MultipoleDet:
     def __call__(self, mult_l, mult_m):
         return self[(mult_l, mult_m)]
 
+    def __eq__(self, other):
+        if (not isinstance(other, type(self))):
+            return False
+        return (self.dist == other.dist and
+                self._multipoles == other._multipoles)
+
     def __iter__(self):
         for (mult_l, mult_m), ts in self._multipoles.items():
             yield mult_l, mult_m, ts
@@ -83,3 +93,209 @@ class MultipoleDet:
 
     def keys(self):
         return self.available_lm
+
+
+class MultipoleAllDets:
+    """This class collects available surfaces with multipole data.
+
+    It works as a dictionary in terms of spherical surface radius,
+    returning a :py:class:`MultipoleDet` object. Iteration is supported,
+    sorted by ascending radius. You can iterate over all the radii and
+    all the available l and m with a nested loop.
+
+    :ivar radii:        Available surface radii.
+    :vartype radii: float
+    :ivar r_outer:      Radius of the outermost detector
+    :vartype r_outer: float
+    :ivar outermost:    Outermost detector
+    :vartype outermost: :py:class:`~MultipoleDet`
+    :ivar available_lm: Available components as tuple (l,m).
+    :vartype available_lm: list of tuples
+    :ivar available_l:  List of available "l".
+    :vartype available_l: list
+    :ivar available_m:  List of available "m".
+    :vartype available_m: list
+    """
+
+    def __init__(self, data):
+        """Data is a list of tuples with structure
+        (multipole_l, multipole_m, extraction_radius, [timeseries])
+        """
+        detectors = {}
+        self.available_lm = set()
+
+        # Populate detectors
+        # detectors is a dictionary with keys the radii and
+        # items that look like ([l, m, [timeseries], ...]).
+        # We accumulate in the list all the ones for the same
+        # radius
+        for mult_l, mult_m, radius, ts in data:
+            # If we don't have the radius yet, let's create an empty list
+            d = detectors.setdefault(radius, [])
+            # Add the new values
+            d.append((mult_l, mult_m, ts))
+            # Tally the available l and m
+            self.available_lm.add((mult_l, mult_m))
+
+        self._detectors = {radius: MultipoleDet(radius, multipoles)
+                           for radius, multipoles in detectors.items()}
+
+        # In Python3 .keys() is not a list
+        self.radii = sorted(list(self._detectors.keys()))
+
+        if len(self.radii) > 0:
+            self.r_outer = self.radii[-1]
+            self.outermost = self._detectors[self.r_outer]
+        #
+        self.available_l = {mult_l for mult_l, _ in self.available_lm}
+        self.available_m = {mult_m for _, mult_m in self.available_lm}
+
+        # Alias
+        self._dets = self._detectors
+
+    def __contains__(self, key):
+        return key in self._dets
+
+    def __getitem__(self, key):
+        return self._dets[key]
+
+    def __iter__(self):
+        for r in self.radii:
+            yield self[r]
+
+    def __eq__(self, other):
+        if (not isinstance(other, type(self))):
+            return False
+        return (self.radii == other.radii and
+                self._dets == other._dets)
+
+    def __len__(self):
+        return len(self._dets)
+
+    def keys(self):
+        return self._dets.keys()
+
+
+# class CactusMultipoleDir:
+#     """This class provides acces to various types of multipole data in a given
+#     simulation directory.
+
+#     Files are lazily loaded.
+
+#     If both h5 and ASCII are present, h5 are preferred. There's no attempt to
+#     combine the two.
+#     """
+
+#     def __init__(self, sd):
+#         # self._vars is a dictionary. The keys are the variables,
+#         # and the items are lists of tuples of the form
+#         # (multipole_l, multipole_m, radius, filename) for text
+#         # files and
+#         # (0, 0, -1, filename) for h5 files (since we have to read
+#         # the content to find the l and m)
+#         self._vars = {}
+
+#         # First, we need to find the multipole files.
+#         # There are text files and h5 files
+#         #
+#         # We use a regular expressions on the name
+#         # The structure is like mp_Psi4_l_m2_r110.69.asc
+#         #
+#         # Let's understand the regexp:
+#         # 0. ^ and $ means that we match the entire name
+#         # 1. We match mp_ followed my the variable name, which is
+#         #    any combination of characters
+#         # 2. We match _l with a number
+#         # 3. We match _m with possibly a minus sign and a number
+#         # 4. We match _r with any combination of numbers with possibly
+#         #    dots
+#         # 5. We possibly match a compression
+#         rx_txt = re.compile(r"""^
+#         mp_([a-zA-Z0-9\[\]_]+)
+#         _l(\d+)
+#         _m([-]?\d+)
+#         _r([0-9.]+)
+#         .asc
+#         (?:.bz2|.gz)?
+#         $""", re.VERBOSE)
+
+#         # For h5 files is easy: it is just the var name
+#         rx_h5 = re.compile(r'^mp_([a-zA-Z0-9\[\]_]+).h5$')
+
+#         for f in sd.allfiles:
+#             filename = os.path.split(f)[1]
+#             matched_h5 = rx_h5.match(filename)
+#             matched_txt = rx_txt.match(filename)
+#             if (matched_h5 is not None):
+#                 variable_name = matched_h5.group(1).lower()
+#                 var_list = self._vars.setdefault(variable_name, [])
+#                 # We are flagging that this h5
+#                 var_list.append((0, 0, -1, filename))
+#             elif (matched_txt is not None):
+#                 variable_name = matched_txt.group(1).lower()
+#                 mult_l = int(matched_txt.group(2))
+#                 mult_m = int(matched_txt.group(3))
+#                 radius = float(matched_txt.group(4))
+#                 var_list = self._vars.setdefault(variable_name, [])
+#                 var_list.append((mult_l, mult_m, radius,
+#                                  filename))
+
+#     def __contains__(self, key):
+#         return str(key).lower() in self._vars
+
+#     def _multipole_from_textfile(self, path):
+#         a = np.loadtxt(path, unpack=True, ndmin=2)
+#         if ((len(a) != 3)):
+#             raise RuntimeError('Wrong format')
+#         mp = a[1] + 1j * a[2]
+#         return timeseries.remove_duplicate_iters(a[0], mp)
+
+#     def _multipoles_from_textfiles(self, mpfiles):
+#         amp = [(mult_l, mult_m, radius, self._multipole_from_textfile(filename))
+#                for mult_l, mult_m, radius, filename in mpfiles]
+
+#         return MultipoleAllDets(amp)
+
+#     def _multipoles_from_h5file(self, mpfile):
+#         mpfiles = h5py.File(mpfile, 'r')
+#         amp = []
+#         # This regexp matched : l(number)_m(-number)_r(number)
+#         fieldname_pattern = re.compile(r'l(\d+)_m([-]?\d+)_r([0-9.]+)')
+
+#         for n in mpfile.keys():
+#             matched = fieldname_pattern.match(n)
+#             if not matched:
+#                 continue
+
+#             mult_l = int(matched.group(1))
+#             mult_m = int(matched.group(2))
+#             radius = float(matched.group(3))
+#             a = mpfile[n][()]
+#             ts = timeseries.TimeSeries(a[:, 0], a[:, 1] + 1j*a[:, 2])
+#             amp.append((mult_l, mult_m, radius, ts))
+
+#     def _multipoles_from_h5files(self, mpfiles):
+#         mult_l = []
+
+#         for filename in mpfiles:
+#             mult_l.extend(self._mp_from_h5file(filename))
+
+#         return MultipoleAllDets(mult_l)
+
+#     def __getitem__(self, key):
+#         k = str(key).lower()
+#         if self._vars[k][2] == -1:  # When the radius is -1 it's h5
+#             return self._multipoles_from_h5files(self._vars[k])
+#         else:
+#             return self._multipoles_from_textfiles(self._vars[k])
+
+#     def get(self, key, default=None):
+#         if key not in self:
+#             return default
+#         return self[key]
+
+#     def keys(self):
+#         return list(self._vars.keys())
+
+#     def __str__(self):
+#         pass
--- tests/test_cactus_multipoles.py
+++ tests/test_cactus_multipoles.py
@@ -10,20 +10,21 @@ from postcactus import cactus_multipoles as mp
 
 class TestCactusMultipoles(unittest.TestCase):
 
+    def setUp(self):
+        # Prepare fake multipoles
+        self.t1 = np.linspace(0, 1, 100)
+        self.t2 = np.linspace(2, 3, 100)
+        self.y1 = np.sin(self.t1)
+        self.y2 = np.sin(self.t2)
+        self.ts1 = ts.TimeSeries(self.t1, self.y1)
+        self.ts2 = ts.TimeSeries(self.t2, self.y2)
+        
     def test_MultipoleDet(self):
 
-        # Prepare fake multipoles
-        t1 = np.linspace(0, 1, 100)
-        t2 = np.linspace(2, 3, 100)
-        y1 = np.sin(t1)
-        y2 = np.sin(t2)
-        ts1 = ts.TimeSeries(t1, y1)
-        ts2 = ts.TimeSeries(t2, y2)
+        ts_comb = ts.combine_ts([self.ts1, self.ts2])
 
-        ts_comb = ts.combine_ts([ts1, ts2])
-
-        data = [(2, 2, ts1), (2, 2, ts2)]
-        data2 = [(2, 2, ts1), (2, -2, ts2)]
+        data = [(2, 2, self.ts1), (2, 2, self.ts2)]
+        data2 = [(2, 2, self.ts1), (2, -2, self.ts2)]
 
         mult1 = mp.MultipoleDet(100, data)
         mult2 = mp.MultipoleDet(100, data2)
@@ -33,8 +34,8 @@ class TestCactusMultipoles(unittest.TestCase):
 
         # test __call__
         self.assertEqual(mult1(2, 2), ts_comb)
-        self.assertEqual(mult2(2, 2), ts1)
-        self.assertEqual(mult2(2, -2), ts2)
+        self.assertEqual(mult2(2, 2), self.ts1)
+        self.assertEqual(mult2(2, -2), self.ts2)
 
         # test available_
         self.assertCountEqual(mult1.available_l, {2})
@@ -47,10 +48,10 @@ class TestCactusMultipoles(unittest.TestCase):
         # test contains
         self.assertIn((2, 2), mult1)
         self.assertIn((2, -2), mult2)
-        self.assertEqual(mult2[(2, 2)], ts1)
+        self.assertEqual(mult2[(2, 2)], self.ts1)
 
         # test_iter
-        expected = [(2, 2, ts1), (2, -2, ts2)]
+        expected = [(2, 2, self.ts1), (2, -2, self.ts2)]
         for data, exp in zip(mult2, expected):
             self.assertCountEqual(data, exp)
 
@@ -59,4 +60,51 @@ class TestCactusMultipoles(unittest.TestCase):
         self.assertEqual(len(mult2), 2)
 
         # test keys()
-        self.assertCountEqual(mult1.keys(), [(2,2)])
+        self.assertCountEqual(mult1.keys(), [(2, 2)])
+
+        # test __eq__()
+        self.assertNotEqual(mult1, mult2)
+        self.assertNotEqual(mult1, 1)
+        self.assertEqual(mult1, mult1)
+
+    def test_MultipoleAllDet(self):
+
+        data = [(2, 2, 100, self.ts1),
+                (2, -2, 150, self.ts2)]
+
+        radii = [100, 150]
+
+        alldets = mp.MultipoleAllDets(data)
+
+        self.assertEqual(alldets.radii, radii)
+        self.assertSetEqual(alldets.available_lm, {(2, 2), (2, -2)})
+
+        # test __contains__
+        self.assertIn(100, alldets)
+
+        # test __getitem__
+        data_single = [(2, 2, self.ts1)]
+        mult_single = mp.MultipoleDet(100, data_single)
+        self.assertEqual(alldets[100], mult_single)
+
+        # test __eq__
+        self.assertEqual(alldets, alldets)
+        self.assertNotEqual(alldets, 1)
+
+        data2 = [(2, 2, 100, self.ts1),
+                 (2, -2, 180, self.ts2)]
+
+        alldets2 = mp.MultipoleAllDets(data2)
+
+        self.assertNotEqual(alldets, alldets2)
+
+
+        # test __iter__
+        for det, r in zip(alldets, radii):
+            self.assertEqual(det.radius, r)
+
+        # test __len__
+        self.assertEqual(len(alldets), 2)
+
+        # keys()
+        self.assertCountEqual(alldets.keys(), radii)
