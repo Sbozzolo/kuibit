6140d0387851b2b36cc7630a2d12a570dbdace8c
Gabriele Bozzola <sbozzolator@gmail.com>
2020-08-31 10:07:23 -0700

    Add spin weighted spherical harmonics

--- docs/gw_utils.rst
+++ docs/gw_utils.rst
@@ -1,4 +1,23 @@
 Gravitational-wave utilities
 ==============================
 
- :ref:`gw_utils_ref:Reference on postcactus.gw_utils`
+:ref:`gw_utils_ref:Reference on postcactus.gw_utils`
+
+
+luminosity_distance_to_redshift
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+Compute the redshift starting from a given luminosity distance in Megaparsec.
+The assumed cosmology is LCDM with no radiation (only matter and dark energy)
+and the default values are the ones provided by the Planck mission.
+
+The computation is based on a numerical inversion, which requires an initial
+guess. The default one (0.1) should work in most scenarios, but it should be
+changed in case of failure.
+
+sYlm
+^^^^
+
+Compute the spin-weighted spherical harmonics using recursion relationships. The
+angles are defined as :math:`\theta` the meridional angle and :math:`\phi` the
+azimulathal one.
--- tests/test_gw_utils.py
+++ tests/test_gw_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from postcactus import gw_utils as gwu
 
-class TestTimeseries(unittest.TestCase):
+class TestGWUtils(unittest.TestCase):
 
     def test_luminosity_distance_to_redshift(self):
 
@@ -13,3 +13,21 @@ class TestTimeseries(unittest.TestCase):
         with self.assertRaises(RuntimeError):
             print(gwu.luminosity_distance_to_redshift(1e15,
                                                       Omega_m=0))
+
+    def test_sYlm(self):
+        # Test values froom kerrgeodesic_gw
+
+        self.assertAlmostEqual(gwu.sYlm(0, -1, 0, 0, 1), 0)
+        self.assertAlmostEqual(gwu.sYlm(0, 1, 2, 0, 1), 0)
+        self.assertAlmostEqual(gwu.sYlm(-2, 2, 1, 1.0, 2.0),
+                               -0.170114676286891 + 0.371707349012686j)
+        self.assertAlmostEqual(gwu.sYlm(-2, 2, 1, 1.5, 2.0),
+                               -0.140181365376761 + 0.306301871434652j)
+        self.assertAlmostEqual(gwu.sYlm(-2, 2, -1, 1.5, 2.0),
+                               -0.121659476911011 - 0.265830806794102j)
+        self.assertAlmostEqual(gwu.sYlm(-2, 2, -2, 1.5, 2.0),
+                               -0.0890098785065999 + 0.103057531674292j)
+        self.assertAlmostEqual(gwu.sYlm(0, 2, 1, 1.5, 2.0),
+                               0.0226845879069160 - 0.0495667288582717j)
+        self.assertAlmostEqual(gwu.sYlm(0, 3, 1, 1.5, 2.0),
+                               -0.130797156679223 + 0.285797001345366j)
