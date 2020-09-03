244ba7a4b3a192e5827484babf288cef8e0aa0eb
Gabriele Bozzola <sbozzolator@gmail.com>
2020-09-03 14:40:18 -0700

    Add antenna_responses

--- postcactus/gw_utils.py
+++ postcactus/gw_utils.py
@@ -3,10 +3,12 @@
 
 """
 
+import datetime
+from collections import namedtuple
+import numpy as np
 import postcactus.unitconv as uc
 from scipy import optimize
 from scipy import integrate
-import numpy as np
 
 
 def luminosity_distance_to_redshift(luminosity_distance,
@@ -145,3 +147,158 @@ def sYlm(ss, ll, mm, theta, phi):
     result = Pm * s_lambda_lm(mult_s, mult_l, mult_m, np.cos(theta))
 
     return complex(result * np.cos(mm * phi), result * np.sin(mm * phi))
+
+
+def antenna_responses(right_ascension, declination, time_utc, polarization=0):
+    """Return the antenna responses for Hanford and Livingston for a given
+    source.
+
+    See, research.physics.illinois.edu/cta/movies/bhbh_sim/wavestrain.html,
+    considering that there is typo in the sign of the second cosine in the F
+    cross.
+
+    utc_time has to have the following formatting: %Y-%m-%d %H:%M,
+    eg 2015-09-14 09:50:45
+
+    Return values are plus and cross responses for Hanford, Livingston, and
+    Virgo.
+
+    :param right_ascension: Right ascension of the source in degrees
+    :type right_ascension: float
+    :param declination: Declination of the source in degrees
+    :type declination: float
+    :param time_utc: UTC time of the event
+    :type declination: str
+    :param polarization: Polarization of the wave
+    :type polarization: float
+
+    :rvalue antenna_pattern: Cross and plus antenna pattern for the different
+    interferometers
+    :rtype: namedtuple with fields hanford, livingston, and virgo
+
+    """
+    # NOTE: This function can make use of some cleanup and double-checking
+    # This code is based on the one written by Yuk Tung Liu.
+
+    # RA and DEC of the GW source:
+    alpha_hours = right_ascension
+    delta_degrees = declination
+
+    # degrees to radian
+    deg_to_rad = np.pi / 180.0
+
+    alpha = alpha_hours * 15.0 * deg_to_rad
+    delta = delta_degrees * deg_to_rad
+
+    # LIGO detector geometry from
+    # http://www.ligo.org/scientists/GW100916/GW100916-geometry.html
+    # and also
+    # http://www.ligo.org/scientists/GRB051103/GRB051103-geometry.php
+    # The two pages are consistent.
+
+    # LIGO Hanford (H) detector geometry
+    # Latitude, Longitude and azimuth of the x-arm
+    # Note: Longitude is measured positively westward from Greenwich
+    # Azimuth is measured from the South point, turning positive to the West.
+    lat_H = (46 + 27.0 / 60 + 19.0 / 3600) * deg_to_rad
+    long_H = (119 + 24.0 / 60 + 28.0 / 3600) * deg_to_rad
+    xazi_H = (180 - 36) * deg_to_rad
+
+    # LIGO Livingston (L) detector geometry
+    # Latitude, Longitude and azimuth of the x-arm
+    lat_L = (30 + 33.0 / 60 + 46.0 / 3600) * deg_to_rad
+    long_L = (90 + 46.0 / 60 + 27.0 / 3600) * deg_to_rad
+    xazi_L = (90 - 18) * deg_to_rad
+
+    # Virgo detector geometry
+    # https://arxiv.org/pdf/1706.09505.pdf (Table 3.1)
+    lat_V = (43 + 37.0/60 + 53.0/3600)*deg_to_rad
+    long_V = -(10 + 30.0/60 + 16.0/3600)*deg_to_rad
+    xazi_V = -(180-19)*deg_to_rad
+
+    # Time of detection of GW150914: 2015-09-14 09:50:45 UTC
+    # The Greenwich sidereal time theta_G is calculated by
+    # the formula at https://en.wikipedia.org/wiki/Sidereal_time
+
+    base_posix_date = datetime.datetime.strptime(
+        "2000-01-01 12:00:00", "%Y-%m-%d %H:%M:%S").timestamp()
+    posix_date = datetime.datetime.strptime(time_utc,
+                                            "%Y-%m-%d %H:%M:%S").timestamp()
+    # Days between DATE and 2000-01-01 12:00
+    D = (posix_date - base_posix_date) / 86400
+    theta_G = 18.697374558 + 24.06570982441908 * D
+    # theta_G mod 24h
+    theta_G = theta_G - np.floor(theta_G / 24.0) * 24
+    theta_G = theta_G * 15 * deg_to_rad
+
+    # Equatorial -> horizontal using the formulae at
+    # https://en.wikipedia.org/wiki/Celestial_coordinate_system
+
+    # Hour angles
+    h_H = theta_G - long_H - alpha
+    h_L = theta_G - long_L - alpha
+    h_V = theta_G - long_V - alpha
+
+    # Zenith distance theta and azimuth A
+    theta_H = np.arccos(
+        np.sin(lat_H) * np.sin(delta) +
+        np.cos(lat_H) * np.cos(delta) * np.cos(h_H))
+    theta_L = np.arccos(
+        np.sin(lat_L) * np.sin(delta) +
+        np.cos(lat_L) * np.cos(delta) * np.cos(h_L))
+    theta_V = np.arccos(
+        np.sin(lat_V) * np.sin(delta) +
+        np.cos(lat_V) * np.cos(delta) * np.cos(h_V))
+
+    A_H = np.arctan2(
+        np.cos(delta) * np.sin(h_H),
+        np.cos(delta) * np.cos(h_H) * np.sin(lat_H) -
+        np.sin(delta) * np.cos(lat_H))
+    phi_H = xazi_H - A_H
+
+    A_L = np.arctan2(
+        np.cos(delta) * np.sin(h_L),
+        np.cos(delta) * np.cos(h_L) * np.sin(lat_L) -
+        np.sin(delta) * np.cos(lat_L))
+    phi_L = xazi_L - A_L
+
+    A_V = np.arctan2(
+        np.cos(delta) * np.sin(h_V),
+        np.cos(delta) * np.cos(h_V) * np.sin(lat_V) -
+        np.sin(delta) * np.cos(lat_V))
+    phi_V = xazi_V - A_V
+
+    # Equations from the website with corrected typo in the sign of F_cross
+    # (It should be + in front of the cosine)
+    Fp_H = (0.5 * (1 + np.cos(theta_H) * np.cos(theta_H))
+            * np.cos(2 * phi_H) * np.cos(2 * polarization)
+            - np.cos(theta_H) * np.sin(2 * phi_H) * np.sin(2 * polarization))
+    Fc_H = (0.5 * (1 + np.cos(theta_H) * np.cos(theta_H))
+            * np.cos(2 * phi_H) * np.sin(2 * polarization)
+            + np.cos(theta_H) * np.sin(2 * phi_H) * np.cos(2 * polarization))
+
+    Fp_L = (0.5 * (1 + np.cos(theta_L) * np.cos(theta_L))
+            * np.cos(2 * phi_L) * np.cos(2 * polarization)
+            - np.cos(theta_L) * np.sin(2 * phi_L) * np.sin(2 * polarization))
+    Fc_L = (0.5 * (1 + np.cos(theta_L) * np.cos(theta_L))
+            * np.cos(2 * phi_L) * np.sin(2 * polarization)
+            + np.cos(theta_L) * np.sin(2 * phi_L) * np.cos(2 * polarization))
+
+    Fp_V = (0.5 * (1 + np.cos(theta_V) * np.cos(theta_V))
+            * np.cos(2 * phi_V) * np.cos(2 * polarization)
+            - np.cos(theta_V) * np.sin(2 * phi_V) * np.sin(2 * polarization))
+    Fc_V = (0.5 * (1 + np.cos(theta_V) * np.cos(theta_V))
+            * np.cos(2 * phi_V) * np.sin(2 * polarization)
+            + np.cos(theta_V) * np.sin(2 * phi_V) * np.cos(2 * polarization))
+
+
+    # This is just a convenience to avoid having to remember the order of
+    # the output
+    # One can access the output as antenna_LIGO.hanford, or
+    # antenna_LIGO['hanford']
+    AntennaResponse = namedtuple("AntennaResponse", "hanford livingston virgo")
+    antenna = AntennaResponse(hanford=(Fc_H, Fp_H),
+                              livingston=(Fc_L, Fp_L),
+                              virgo=(Fc_V, Fp_V))
+
+    return antenna
--- tests/test_gw_utils.py
+++ tests/test_gw_utils.py
@@ -31,3 +31,17 @@ class TestGWUtils(unittest.TestCase):
                                0.0226845879069160 - 0.0495667288582717j)
         self.assertAlmostEqual(gwu.sYlm(0, 3, 1, 1.5, 2.0),
                                -0.130797156679223 + 0.285797001345366j)
+
+    def test_antenna_responses(self):
+
+        antenna_gw150914 = gwu.antenna_responses(8, -70,
+                                                 "2015-09-14 09:50:45")
+
+        # This test is extremely weak: the numbers that are here were
+        # obtained with the function itself
+        self.assertAlmostEqual(antenna_gw150914.hanford[0], 0.173418558)
+        self.assertAlmostEqual(antenna_gw150914.hanford[1], 0.734266762)
+        self.assertAlmostEqual(antenna_gw150914.livingston[0], 0.030376784)
+        self.assertAlmostEqual(antenna_gw150914.livingston[1], -0.569292709)
+        self.assertAlmostEqual(antenna_gw150914.virgo[0], -0.11486789)
+        self.assertAlmostEqual(antenna_gw150914.virgo[1], 0.57442590)
