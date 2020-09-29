64ee19fd1791541fd934282c9bd45597c3bfd909
Gabriele Bozzola <sbozzolator@gmail.com>
2020-09-28 18:20:10 -0700

    Bump to version 3.0.0a4

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Gabriele Bozzola and Wolfgang Kastaun'
 author = 'Gabriele Bozzola and Wolfgang Kastaun'
 
 # The full version, including alpha/beta/rc tags
-release = '3.0.0a3'
+release = '3.0.0a4'
 
 
 # -- General configuration ---------------------------------------------------
