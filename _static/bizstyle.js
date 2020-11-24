fd6ace52184e87cffd70fbe0f3af94181eb9b3f0
GitHub Action <action@github.com>
2020-11-24 18:27:20 +0000

    Update documentation

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("PostCactus 3.0.0a9 documentation");
+        $("li.nav-item-0 a").text("PostCactus 3.0.0a10 documentation");
     }
 });
