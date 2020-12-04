421b143d16361047270c4219a9a10a05ee63261e
Gabriele Bozzola <sbozzolator@gmail.com>
2020-12-04 11:24:55 -0700

    Add copyright header in grid_data

--- postcactus/grid_data.py
+++ postcactus/grid_data.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+# Copyright (C) 2020 Gabriele Bozzola, Wolfgang Kastaun
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <https://www.gnu.org/licenses/>.
+
 """The :py:mod:`~.grid_data` module provides representations of data on
 uniform grids as well as for data on refined grid hirachies. Standard
 arithmetic operations are supported for those data grids, further methods
