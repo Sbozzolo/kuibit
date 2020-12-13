30f1be1a00a3737710fbe4e822087c803af5fe52
Gabriele Bozzola <sbozzolator@gmail.com>
2020-12-13 16:21:01 -0700

    Add missing copyright header

--- postcactus/__init__.py
+++ postcactus/__init__.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
+# Copyright (C) 2020 Gabriele Bozzola, Wolfgang Kastaun
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <https://www.gnu.org/licenses/>.
+
 """PostCactus provides utilities to post-process simulations performed with the
-Einstein Toolkit.
+Einstein Toolkit (or any Cactus-based code).
 
 """
 
