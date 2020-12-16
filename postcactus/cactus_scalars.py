917a22a8721d73a1b1231ebc7b462fcaa5b5bea9
Erik Wessel <ekwessel@mac.com>
2020-12-15 17:45:51 -0800

    Un-did test change

--- postcactus/cactus_scalars.py
+++ postcactus/cactus_scalars.py
@@ -22,7 +22,6 @@ is normally not used directly, but from the :py:mod:`~.simdir` module.
 The data loaded by this module is represented as
 :py:class:`~.TimeSeries` objects.
 """
-#test change
 
 import os
 import re
