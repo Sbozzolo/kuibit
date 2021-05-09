bb1f854747963d90951a18888ac32f2b0d191834
GitHub Action <action@github.com>
2021-05-09 16:48:47 +0000

    Update documentation

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("kuibit 1.1.1 documentation");
+        $("li.nav-item-0 a").text("kuibit 1.2.0b0 documentation");
     }
 });
