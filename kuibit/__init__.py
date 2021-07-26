1391fcec2f719dab5870f9f9ab8674b235199a11
Gabriele Bozzola <sbozzolator@gmail.com>
2021-07-26 14:06:13 -0700

    Bump to 1.3.0-dev1

--- kuibit/__init__.py
+++ kuibit/__init__.py
@@ -20,7 +20,7 @@ Einstein Toolkit (or any Cactus-based code).
 
 """
 
-__version__ = "1.3.0-dev"
+__version__ = "1.3.0-dev1"
 
 __bibtex__ = """\
 @article{kuibit,
--- tests/test_kuibit.py
+++ tests/test_kuibit.py
@@ -22,7 +22,7 @@ from kuibit import __bibtex__, __version__
 
 class Testkuibit(unittest.TestCase):
     def test_version(self):
-        self.assertEqual(__version__, "1.3.0-dev")
+        self.assertEqual(__version__, "1.3.0-dev1")
 
     def test_bibtex(self):
         self.assertIn("Bozzola", __bibtex__)
