0a3dcb5372336d0cbdfff26410d4d4cbfcb6cf9f
Gabriele Bozzola <sbozzolator@gmail.com>
2022-06-14 16:06:38 -0700

    Bump to 1.3.5

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020-2022, Gabriele Bozzola'
 author = 'Gabriele Bozzola'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.4'
+release = '1.3.5'
 
 
 # -- General configuration ---------------------------------------------------
