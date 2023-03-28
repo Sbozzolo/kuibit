32a8e99101e7505bbfeef58c299cb9a63b44d5a0
Gabriele Bozzola <sbozzolator@gmail.com>
2023-03-28 12:20:59 -0700

    Lint: clean up docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,17 +13,17 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'kuibit'
-copyright = '2020-2023, Gabriele Bozzola'
-author = 'Gabriele Bozzola'
+project = "kuibit"
+copyright = "2020-2023, Gabriele Bozzola"
+author = "Gabriele Bozzola"
 
 # The full version, including alpha/beta/rc tags
-release = '1.4.0-dev2'
+release = "1.4.0-dev2"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,56 +31,63 @@ release = '1.4.0-dev2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-              "sphinx_autodoc_typehints",
-              'sphinx.ext.autosectionlabel',
-              'sphinx.ext.mathjax',
-              'sphinx.ext.viewcode',
-              'nbsphinx',
-              'sphinxcontrib.bibtex',
-              'sphinxcontrib.citations',
-              ]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "nbsphinx",
+    "sphinxcontrib.bibtex",
+    "sphinxcontrib.citations",
+]
 
 autosectionlabel_prefix_document = True
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-autoclass_content = 'both'
+autoclass_content = "both"
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = "bizstyle"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
-    'css/custom.css',
+    "css/custom.css",
 ]
 
-html_logo = '../logo.png'
+html_logo = "../logo.png"
 
 html_show_sourcelink = False
 
 html_sidebars = {
-   '**': ['localtoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html', 'versions.html'],
+    "**": [
+        "localtoc.html",
+        "relations.html",
+        "sourcelink.html",
+        "searchbox.html",
+        "versions.html",
+    ],
 }
 
-html_context = {'versions': ['1.3.6']}
+html_context = {"versions": ["1.3.6"]}
 
 html_theme_options = {
     "maincolor": "#228B22",
