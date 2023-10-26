e12773f77fe030bd4889ed3e3a4714588a96cb03
Gabriele Bozzola <sbozzolator@gmail.com>
2023-10-26 07:33:29 -0700

    Import tikzplotlib conditionally

--- kuibit/visualize_matplotlib.py
+++ kuibit/visualize_matplotlib.py
@@ -75,7 +75,6 @@ from typing import Any, Dict, Optional
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import tikzplotlib
 from matplotlib.patches import Rectangle
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from mpl_toolkits.mplot3d import Axes3D
@@ -433,6 +432,7 @@ def save(
 
     """
     if os.path.splitext(outputpath)[-1] == ".tikz":
+        import tikzplotlib
         # If clean_figure is True, we extract from kwargs those argument
         # that tikzplotlib.clean_figure would take. For this, we need to
         # know what argument that function takes.
