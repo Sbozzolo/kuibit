92408d543e48fbf20fb0d84ad84063a307c8efa4
Gabriele Bozzola <sbozzolator@gmail.com>
2024-06-16 16:12:55 -0700

    CI: Update documentation pipeline to Python 3.11

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ html_sidebars = {
     ],
 }
 
-html_context = {"versions": ["1.3.6", "1.4.0"]}
+html_context = {"versions": ["1.3.6", "1.4.0", "1.4.1"]}
 
 html_theme_options = {
     "maincolor": "#228B22",
