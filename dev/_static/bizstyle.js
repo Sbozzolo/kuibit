4ea22ba64bd68f446bf8b3446ac5616879aab699
GitHub Action <action@github.com>
2025-07-03 04:04:11 +0000

    Update documentation

--- dev/_static/bizstyle.js
+++ dev/_static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "kuibit 1.5.0 documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "kuibit 1.5.1 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
